8989c7d90cadb5d15fde08b3015c992956e39f7a
Toborro <zephyrthewest@gmail.com>
2018-10-22 19:41:45 -0400

    third commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,7 +19,7 @@ function populateButtons(searchArray,classToAdd,areaToAddTo){
 $(document).on('click','.searchButton',function(){
     $('#searches').empty();
     var type = $(this).data('type');
-    var queryUrl = 'http://api.giphy.com/v1/gifs/search?q='+type+'&api_key=uC2Nk4cLwCk6MOsMnHZnwOfOlZMyU4P5&limit=10';
+    var queryUrl = 'https://api.giphy.com/v1/gifs/search?q='+type+'&api_key=uC2Nk4cLwCk6MOsMnHZnwOfOlZMyU4P5&limit=10';
     $.ajax({url:queryUrl,method:'GET'})
         .done(function(response){
         for(var i=0; i<response.data.length;i++){
