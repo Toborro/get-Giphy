0c2786cd74f94a9cef94bc8707dc58670113a003
Toborro <zephyrthewest@gmail.com>
2018-10-22 19:30:56 -0400

    second commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,9 +3,6 @@ $(function() {
     console.log("Page loaded");
 })
 
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=uC2Nk4cLwCk6MOsMnHZnwOfOlZMyU4P5&limit=5");
-xhr.done(function(data) { console.log("success got data", data); });
-
 var searchArray = ['Dog','Cat','Bird'];
 
 function populateButtons(searchArray,classToAdd,areaToAddTo){
